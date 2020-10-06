d3aa90fe9a4787d3b968a2a9701a33c5615dfad2
Oscar España <espaaoscar@gmail.com>
2020-10-06 15:54:54 -0500

    Use of useEffect for requesting images

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 
 const GifGrid = ({category}) => {
+
+    const [count, setCount] = useState(0);
+
+    useEffect(()=>{
+        getGrids();
+    }, [])
 
     const getGrids = async()=>{
         const url ='https://api.giphy.com/v1/gifs/search?q=Rick and Morty&limit=10&api_key=TCQFCpq5FTQE0QxW0KCnD3xeqaZU2Spa'
@@ -17,11 +23,12 @@ const GifGrid = ({category}) => {
         console.log(gifs);
     }
 
-    getGrids();
 
     return (
         <div>
             <h3>{category}</h3>
+            <h4>{count}</h4>
+            <button onClick={()=>setCount(count+1)}></button>
         </div>
     )
 }
