ccb222e746a340c66788da3231d7e2c6e59dbcd0
Oscar España <espaaoscar@gmail.com>
2020-10-07 07:47:20 -0500

    The hook useFetchGifs was modified to obtain the images and load flag

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,32 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useFetchGifs } from "../hooks/useFetchGifs";
 import GifGridItem from "./GifGridItem";
-// import {getGifs} from '../helpers/getGifs'
 
 const GifGrid = ({ category }) => {
   
-  // const [images, setImages] = useState([]);
-
-  // useEffect(() => {
-  //   getGifs(category)
-  //     .then(imgs => setImages(imgs))
-    
-  // }, [category]);
-  const {loading}= useFetchGifs();
-
+  const {data:images, loading}= useFetchGifs(category);
 
   return (
     <>
       <h3>{category}</h3>
-
       {
-        loading ? 'Cargando...' : 'Data cargada'
+        loading && <p>Loading...</p>
       }
-      {/* <div className="card-grid">
+      <div className="card-grid">
         {images.map((img) => (
           <GifGridItem key={img.id} {...img} />
         ))}
-      </div> */}
+      </div>
     </>
   );
 };
