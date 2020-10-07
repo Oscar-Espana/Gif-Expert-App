5d6f5841362a92e454e00b2f3fd05ec18266f103
Oscar España <espaaoscar@gmail.com>
2020-10-07 09:21:46 -0500

    creation of the production build and rename it with the name docs

---
+++ docs/precache-manifest.81429bd1df1edeafa15ec9983181a5a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1c8823b6ca62ebe6ee95bf9768cc5c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79377117a87c0ac0bf90",
+    "url": "/static/css/main.6a9a9608.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79377117a87c0ac0bf90",
+    "url": "/static/js/main.6b646d3a.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
